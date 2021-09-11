5071fad0417aaf5c3eb61c9747de7217bce0a78b
johanne <johanne_riis@hotmail.com>
2021-09-11 19:56:25 +0200

    First Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!");
+        System.out.println("Her er en linje mere, dette er linje 2");
+        System.out.println("og en trejdie linje");
+
+        System.out.println("linje 1.1\n Linje1.2\n" + "linje 1.3");
+
+        System.out.println("""
+                linje 1
+                        linje 2
+                linje 3
+                """);
+    }
+}
